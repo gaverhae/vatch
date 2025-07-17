c88f82ce3e2896f7d6354addc529c434b0bcbfaf
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-07-17 14:01:02 +0200

    init-nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+  pkgs = import ./nix/nixpkgs.nix {};
+  getFlake = url: (builtins.getFlake url).packages.${pkgs.system}.default;
+in
+pkgs.mkShell {
+  LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+  buildInputs = with pkgs; [
+    bash
+    cacert
+    curl
+    jq
+  ];
+}
